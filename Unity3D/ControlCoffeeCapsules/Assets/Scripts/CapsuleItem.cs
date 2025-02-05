73e81417a8c003c53365a4c4e5827054b45f4934
Oscar Devs <ohernandezba@gmail.com>
2025-02-05 16:29:37 +0100

    edit date

--- Unity3D/ControlCoffeeCapsules/Assets/Scripts/CapsuleItem.cs
+++ Unity3D/ControlCoffeeCapsules/Assets/Scripts/CapsuleItem.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using System;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,19 +7,46 @@ using UnityEngine.UI;
 public class CapsuleItem : MonoBehaviour
 {
     [SerializeField] private Text dateText;
+    [SerializeField] private TMP_InputField dateInputField;
     [SerializeField] private JsonDateTime date;
     [SerializeField] private Button delButton;
+    [SerializeField] private Button editButton;
 
     public UnityEvent<JsonDateTime> OnDeletedCapsuleItem;
 
     private void OnEnable()
     {
         delButton.onClick.AddListener(OnDelButtonClick);
+        editButton.onClick.AddListener(OnEditButtonClick);
+        dateInputField.onSubmit.AddListener(s => Debug.Log($"onSubmit {s}"));
+        dateInputField.onEndEdit.AddListener(s => Debug.Log($"onEndEdit {s}"));
+        dateInputField.onValueChanged.AddListener(s => Debug.Log($"onValueChanged {s}"));
+    }
+
+    private void OnDisable()
+    {
+        delButton.onClick.RemoveListener(OnDelButtonClick);
+        editButton.onClick.RemoveListener(OnEditButtonClick);
     }
 
     private void OnDelButtonClick()
     {
         OnDeletedCapsuleItem.Invoke(date);
+    }
+
+    private void OnEditButtonClick()
+    {
+        dateInputField.text = dateText.text;
+        EditDate();
+    }
+
+    private void EditDate()
+    {
+        dateText.gameObject.SetActive(false);
+        dateInputField.gameObject.SetActive(true);
+        dateInputField.Select();
+        editButton.gameObject.SetActive(false);
+        delButton.gameObject.SetActive(false);
     }
 
     public void SetDate(JsonDateTime date)
