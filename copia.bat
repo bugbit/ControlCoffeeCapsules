725fba801de9802c2d60a66ec7969aa5584e8886
OscarDevNet <ohernandezba@gmail.com>
2025-10-08 11:40:22 +0200

    Create copia.bat

---
+++ copia.bat
@@ -0,0 +1,6 @@
+cd D:\Proyectos\oscar\ControlCoffeeCapsules
+
+del F:\Proyectos\repos\Oscar\ControlCoffeeCapsules.zip
+"C:\Program Files\7-Zip\7z.exe" a -tzip -xr!Library F:\Proyectos\repos\Oscar\ControlCoffeeCapsules.zip D:\Proyectos\oscar\ControlCoffeeCapsules\*
+
+pause
